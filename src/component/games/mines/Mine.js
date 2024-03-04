1ce236d04a458ee200b79f9b42342ad8402414bb
sangharsh <sotumotghare@gmail.com>
2024-03-04 14:01:32 +0530

    fixed rendering gems and mine with sound effect

--- src/component/games/mines/Mine.js
+++ src/component/games/mines/Mine.js
@@ -1,73 +1,81 @@
 import React, { useState, useEffect } from "react";
-import "./Mine.css";
 import gem from "../../assets/gem.svg";
 import mine from "../../assets/mine.svg";
+import "./Mine.css";
+import gemSoundEffect from '../../audio/gemCollect1.mp3';
+import explosionSoundEffect from '../../audio/crash.mp3';
 
 const Mine = () => {
   const rows = 5;
   const columns = 5;
 
-  // Generate an array with all box IDs
   const allBoxIds = Array.from({ length: rows * columns }, (_, index) => index);
 
-  // State for revealed box IDs
   const [revealedBoxIds, setRevealedBoxIds] = useState([]);
-
-  // State for shuffled box IDs
   const [shuffledBoxIds, setShuffledBoxIds] = useState([]);
+  const [viewBoxIds, setViewBoxIds] = useState([]);
+  const [mineImage, setMineImage] = useState(null);
 
-  console.log("revealedBoxIds", revealedBoxIds)
-
-  // Fisher-Yates shuffle function
   const fisherYatesShuffle = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
-    console.log(array)
     return array;
   };
 
   useEffect(() => {
-    // Shuffle the array of all box IDs
     const shuffledIds = fisherYatesShuffle([...allBoxIds]);
-
-    // Select the first 5 box IDs from the shuffled array
     const selectedIds = shuffledIds.slice(0, 5);
+    const remainingShuffledIds = shuffledIds.slice(5);
 
-    // Set the shuffled and selected box IDs in state
-    setShuffledBoxIds(shuffledIds);
+    setShuffledBoxIds(remainingShuffledIds);
     setRevealedBoxIds(selectedIds);
-
-    // Add your custom logic here
-  }, []); // Empty dependency array ensures this effect runs once on component mount
+  }, []);
 
   const handleBoxClick = (boxId) => {
-    console.log(`Clicked on box with id: ${boxId}`);
-    // Add your custom logic here
-  };
+    if (!viewBoxIds.includes(boxId)) {
+        setViewBoxIds((prevIds) => [...prevIds, boxId]);
+
+        if(shuffledBoxIds.includes(boxId)){
+          console.log('handleBoxClick found shuffled boxId!!')
+          const audio = new Audio(gemSoundEffect);
+          audio.play()
+        } else if(revealedBoxIds.includes(boxId)){
+          const audioData = new Audio(explosionSoundEffect)
+          audioData.play()
+        }
+    }
+};
+
+  console.log("shuffleBox id is ", shuffledBoxIds);
+  console.log("revealBox id is ", revealedBoxIds);
+  console.log("viewBox id is ", viewBoxIds);
 
   const renderRow = (rowIndex) => {
     const boxes = [];
     for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
       const boxId = rowIndex * columns + columnIndex;
       const isRevealed = revealedBoxIds.includes(boxId);
-      const isShuffled = shuffledBoxIds.includes(boxId);
+
+      const isClickedAndNotRevealedMine =
+      viewBoxIds.includes(boxId) && revealedBoxIds.includes(boxId);
+      const isClickedAndNotRevealed =
+        viewBoxIds.includes(boxId) && !revealedBoxIds.includes(boxId);
 
       boxes.push(
         <div
           key={`box${boxId}`}
           id={`box${boxId}`}
-          className={`box${columnIndex} boxes ${
-            isRevealed ? "boxes-reveal" : ""
-          } ${isShuffled ? "boxes-shuffled" : ""}`}
+          className={`box${columnIndex} ${viewBoxIds.includes(boxId) ? 'boxes-reveal' : 'boxes'}`}
           onClick={() => handleBoxClick(boxId)}
         >
-          {/* {isRevealed ? (
-            <img src={mine} alt="mine" className="mine-img" />
-          ) : (
+          {isClickedAndNotRevealed && (
             <img src={gem} alt="gem" className="gem-img" />
-          )} */}
+          )}
+          {isClickedAndNotRevealedMine && (
+            <img src={mine} alt="mine" className="mine-img" />
+          )}
         </div>
       );
     }
