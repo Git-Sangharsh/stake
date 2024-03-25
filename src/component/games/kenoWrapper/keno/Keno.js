e9c7413d15e203a2697a6589e1a37e97795c40e1
sangharsh <sangharshdev69@gmail.com>
2024-03-26 01:00:52 +0530

    added sound effect onClick

--- src/component/games/kenoWrapper/keno/Keno.js
+++ src/component/games/kenoWrapper/keno/Keno.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import "./Keno.css";
+import kenoClickEffect from "../../../audio/kenoClick2.mp3";
 
 const Keno = () => {
   const [selcetedBoxes, setSelctedBoxes] = useState([]);
+
   const boxes = Array.from({ length: 40 }, (_, index) => index + 1);
 
   const handleSelectedKenoBoxes = (box) => {
@@ -11,17 +13,32 @@ const Keno = () => {
       if (prevSelectedBoxes.includes(box)) {
         return prevSelectedBoxes.filter((selectedBox) => selectedBox !== box);
       } else {
-        return [...prevSelectedBoxes, box];
+        if (selcetedBoxes.length < 11) {
+          const audio = new Audio(kenoClickEffect);
+          audio.volume = 0.5;
+
+          audio.play();
+
+          return [...prevSelectedBoxes, box];
+        } else {
+          console.log("Max boxes is reacched");
+          return prevSelectedBoxes;
+        }
       }
     });
   };
+
+  console.log(selcetedBoxes);
 
   return (
     <div className="keno">
       <div className="keno-grid">
         {boxes.map((box) => (
-          <div key={box}
-          className={`keno-box ${selcetedBoxes.includes(box) ? 'selected-keno-box' : ''}`}
+          <div
+            key={box}
+            className={`keno-box ${
+              selcetedBoxes.includes(box) ? "selected-keno-box" : ""
+            }`}
           >
             <div
               className="keno-box-number"
