44f25629e904d6a5fd7c8898d96a338e6346531a
sangharsh <sangharshdev69@gmail.com>
2024-03-26 01:19:58 +0530

    commit

--- src/component/games/kenoWrapper/keno/Keno.js
+++ src/component/games/kenoWrapper/keno/Keno.js
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Keno.css";
 import kenoClickEffect from "../../../audio/kenoClick2.mp3";
 
 const Keno = () => {
-  const [selcetedBoxes, setSelctedBoxes] = useState([]);
+  const [selectedBoxes, setSelectedBoxes] = useState([]);
+  const [selectionLimitReached, setSelectionLimitReached] = useState(false);
 
   const boxes = Array.from({ length: 40 }, (_, index) => index + 1);
 
+  useEffect(() => {
+    if (selectedBoxes.length >= 11) {
+      setSelectionLimitReached(true);
+    } else {
+      setSelectionLimitReached(false);
+    }
+  }, [selectedBoxes]);
+
   const handleSelectedKenoBoxes = (box) => {
     console.log("Clicked box id:", box);
-    setSelctedBoxes((prevSelectedBoxes) => {
+    setSelectedBoxes((prevSelectedBoxes) => {
       if (prevSelectedBoxes.includes(box)) {
         return prevSelectedBoxes.filter((selectedBox) => selectedBox !== box);
       } else {
-        if (selcetedBoxes.length < 11) {
+        if (selectedBoxes.length < 11) {
           const audio = new Audio(kenoClickEffect);
           audio.volume = 0.5;
-
           audio.play();
-
           return [...prevSelectedBoxes, box];
         } else {
           console.log("Max boxes is reacched");
@@ -28,16 +35,22 @@ const Keno = () => {
     });
   };
 
-  console.log(selcetedBoxes);
+  // console.log(selcetedBoxes);
+  console.log(selectionLimitReached);
 
   return (
     <div className="keno">
       <div className="keno-grid">
+        <div
+          className={
+            selectionLimitReached ? "select-limit-reached-grid-bg" : "none"
+          }
+        ></div>
         {boxes.map((box) => (
           <div
             key={box}
             className={`keno-box ${
-              selcetedBoxes.includes(box) ? "selected-keno-box" : ""
+              selectedBoxes.includes(box) ? "selected-keno-box" : ""
             }`}
           >
             <div
