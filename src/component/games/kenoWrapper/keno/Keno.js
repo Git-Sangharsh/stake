197ebb7c801db515472bcc7507b0fcec44e16da3
sangharsh <sangharshdev69@gmail.com>
2024-03-26 00:36:26 +0530

    commit

--- src/component/games/kenoWrapper/keno/Keno.js
+++ src/component/games/kenoWrapper/keno/Keno.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import './Keno.css';
+import React from "react";
+import "./Keno.css";
 
 const Keno = () => {
   const boxes = Array.from({ length: 40 }, (_, index) => index + 1);
 
   return (
-    <div className='keno'>
-      <div className='keno-grid'>
-        {boxes.map(box => (
-          <div key={box} className='keno-box'></div>
+    <div className="keno">
+      <div className="keno-grid">
+        {boxes.map((box) => (
+          <div key={box} className="keno-box">
+            <div className="keno-box-number">{box}</div>
+          </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default Keno;
