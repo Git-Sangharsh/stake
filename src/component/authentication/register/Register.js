ca8402b34066b51183fb966ac973288e1941217a
sangharsh <sangharshdev69@gmail.com>
2024-04-09 18:40:20 +0530

    commit

--- src/component/authentication/register/Register.js
+++ src/component/authentication/register/Register.js
@@ -1,64 +1,84 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Register.css";
 import CloseIcon from "@mui/icons-material/Close";
-import LoginIcon from '@mui/icons-material/Login';
+import LoginIcon from "@mui/icons-material/Login";
 import { useDispatch, useSelector } from "react-redux";
 import emailjs from "@emailjs/browser";
 
-
 const Register = () => {
-    const dispatch = useDispatch();
-    const viewRegister = useSelector((state) => state.viewRegister);
+  const dispatch = useDispatch();
 
-    const [inputEmail, setInputEmail] = useState("");
+  const viewRegister = useSelector((state) => state.viewRegister);
 
+  const [inputEmail, setInputEmail] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
 
-    const closeRegister = () => {
-        dispatch({ type: "SET_VIEW_REGISTER", payload: false });
-    };
+  const genrateVerificationCode = () => {
+    const code = Math.floor(1000 + Math.random() * 9000); //genrating random number
+    setVerificationCode(code); 
+  };
 
-    const handleEmailInput = (e) => {
-        setInputEmail(e.target.value);
-    }
+  const closeRegister = () => {
+    dispatch({ type: "SET_VIEW_REGISTER", payload: false });
+  };
 
-    const handleVerifyEmail = () => {
-        console.log(inputEmail)
-        emailjs
-        .send(
-            "service_kkmh1u7",
-            "template_atl8jmp",
-            { email: inputEmail, message: "This Shit Is working" },
-            "GAP3a0IvtBETeKKPw"
-        )
-        .then(
-            (result) => {
-                console.log(result.text);
-                alert("Email Sent");
-                setInputEmail("");
-            },
-            (error) => {
-                console.log(error, error.text);
-            }
-        );
-    }
+  const handleEmailInput = (e) => {
+    setInputEmail(e.target.value);
+    genrateVerificationCode(); // Generate the verification code
+  };
+
+  const handleVerifyEmail = () => {
+    console.log(verificationCode);
+    emailjs
+      .send(
+        "service_kkmh1u7",
+        "template_atl8jmp",
+        { email: inputEmail, message: `Your verification code is: ${verificationCode}` },
+        "GAP3a0IvtBETeKKPw"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Email Sent");
+          setInputEmail("");
+        },
+        (error) => {
+          console.log(error, error.text);
+        }
+      );
+  };
+
+  useEffect(() => {
+    console.log(verificationCode);
+  }, [verificationCode]);
   return (
     <>
       {viewRegister && (
         <div className="register-blur-wrapper">
           <div className="register-wrapper">
             <div className="register-header">
-              <h4 className="signin-header"> <LoginIcon className="wallet-icon"/> Sign In</h4>
+              <h4 className="signin-header">
+                {" "}
+                <LoginIcon className="wallet-icon" /> Sign In
+              </h4>
               <CloseIcon className="user-icon" onClick={closeRegister} />
             </div>
             <div className="register-email-input-div">
               <h3 className="register-email-input-header">Email</h3>
-              <input type="text" id="emailForm" className="register-email-input" onChange={handleEmailInput}/>
+              <input
+                type="text"
+                id="emailForm"
+                className="register-email-input"
+                onChange={handleEmailInput}
+              />
             </div>
-            {/* <div className="register-email-input-div">
+            <div className="register-email-input-div">
               <h3 className="register-email-input-header">Verification Code</h3>
               <input type="text" className="register-email-input" />
-            </div> */}
-            <div className="deposit-btn" onClick={handleVerifyEmail}>Verify</div>
+            </div>
+            <div className="deposit-btn" onClick={handleVerifyEmail}>
+              Verify
+            </div>
           </div>
         </div>
       )}
