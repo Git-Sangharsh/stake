03233fdce9f1628f0af9ef151824786ea7d38427
sangharsh <sangharshdev69@gmail.com>
2024-04-22 15:40:36 +0530

    created Sign in component

--- src/component/authentication/signin/Signin.js
+++ src/component/authentication/signin/Signin.js
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./Signin.css";
 import "../register/Register.css";
 import CloseIcon from "@mui/icons-material/Close";
 import LoginIcon from "@mui/icons-material/Login";
+import axios from "axios";
 const Signin = () => {
+  const dispatch = useDispatch();
   const viewSignin = useSelector((state) => state.viewSignin);
-  console.log("", viewSignin);
   const [emailInput, setEmailInput] = useState("");
+  const [passInput, setPassInput] = useState("");
 
   const closeSignIn = () => {
-
-  }
+    dispatch({ type: "SET_VIEW_SIGNIN", payload: false });
+    if (viewSignin) {
+      dispatch({ type: "SET_VIEW_REGISTER", payload: false });
+    }
+  };
 
   const handleEmailInput = (e) => {
-    setEmailInput(e.target.value)
-  }
+    setEmailInput(e.target.value);
+  };
 
+  const handlePassInput = (e) => {
+    setPassInput(e.target.value);
+  }
+  const handleSigninBtn = () => {
+    console.log("email input is ", emailInput, passInput)
+    
+  }
   return (
     <>
       {viewSignin && (
@@ -32,27 +44,28 @@ const Signin = () => {
               />
             </div>
             <div className="register-email-input-div">
-                <h3 className="register-email-input-header">Email</h3>
-                <input
-                  type="email"
-                  id="emailForm"
-                  className="register-email-input"
-                  onChange={handleEmailInput}
-                  // disabled={showVerificationInput}
+              <h3 className="register-email-input-header">Email</h3>
+              <input
+                type="email"
+                id="emailForm"
+                className="register-email-input"
+                onChange={handleEmailInput}
+                // disabled={showVerificationInput}
                 //   style={{
                 //     pointerEvents: showVerificationInput ? "none" : "auto",
                 //   }}
-                />
-              </div>
-              <div className="register-email-input-div">
-                <h3 className="register-email-input-header">Password</h3>
-                <input
-                  type="password"
-                  id="emailForm"
-                  className="register-email-input"
-                  onChange={handleEmailInput}
-                />
-              </div>
+              />
+            </div>
+            <div className="register-email-input-div">
+              <h3 className="register-email-input-header">Password</h3>
+              <input
+                type="password"
+                id="emailForm"
+                className="register-email-input"
+                onChange={handlePassInput}
+              />
+            </div>
+            <button className="deposit-btn btn-green btn-padding" onClick={handleSigninBtn}>Sign In</button>
           </div>
         </div>
       )}
