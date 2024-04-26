987f9b9678251b43956ba924e59eacdabddc8e7c
sangharsh <sangharshdev69@gmail.com>
2024-04-27 00:35:01 +0530

    added keyPressDown to bet

--- src/component/games/DiceWrapper/dice/Dice.js
+++ src/component/games/DiceWrapper/dice/Dice.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./Dice.css";
 import diceAudio from "../../../audio/diceAudio.mp3";
+import hexagon from "../../../assets/hexagon.svg";
 
 const Dice = () => {
   const [value, setValue] = useState(50);
 
   useEffect(() => {
-    // Update the CSS custom property for dynamic coloring
     document.documentElement.style.setProperty("--thumb-pos", `${value / 100}`);
   }, [value]);
 
@@ -14,11 +14,29 @@ const Dice = () => {
     const newValue = parseFloat(event.target.value);
     setValue(newValue);
 
-    // Play dice audio
     const audio = new Audio(diceAudio);
     audio.play();
   };
 
+  const genrateDiceFloat = () => {
+    const diceFloat = Math.random();
+    const fitNumber = diceFloat * 100;
+    console.log(fitNumber.toFixed(2));
+  }
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if(e.code === "Space"){
+        genrateDiceFloat();
+      }
+      window.addEventListener("keydown", handleKeyPress);
+
+      return () => {
+        window.removeEventListener("keydown", handleKeyPress);
+      }
+    }
+
+  }, [])
   return (
     <div className="dice">
       <div className="child-dice">
@@ -41,8 +59,9 @@ const Dice = () => {
           />
         </div>
       </div>
-      <h1>Selected value is </h1>
-      <h1>{value.toFixed(2)}</h1>
+      <h1>Selected value is {value.toFixed(2)}</h1>
+      <img src={hexagon} alt="" />
+      <button onClick={genrateDiceFloat}>Click To Roll Dice</button>
     </div>
   );
 };
