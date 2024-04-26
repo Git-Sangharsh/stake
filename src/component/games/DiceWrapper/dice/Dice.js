60eba32e71fa4e1d834cdfaa6c4978e911f6e782
sangharsh <sangharshdev69@gmail.com>
2024-04-27 00:20:05 +0530

    commit

--- src/component/games/DiceWrapper/dice/Dice.js
+++ src/component/games/DiceWrapper/dice/Dice.js
@@ -7,14 +7,11 @@ const Dice = () => {
 
   useEffect(() => {
     // Update the CSS custom property for dynamic coloring
-    document.documentElement.style.setProperty(
-      "--thumb-pos",
-      `${value / 100}`
-    );
+    document.documentElement.style.setProperty("--thumb-pos", `${value / 100}`);
   }, [value]);
 
   const handleInputChange = (event) => {
-    const newValue = event.target.value;
+    const newValue = parseFloat(event.target.value);
     setValue(newValue);
 
     // Play dice audio
@@ -24,16 +21,28 @@ const Dice = () => {
 
   return (
     <div className="dice">
-      <div className="parent-dice-range">
-        <input
-          className="dice-range"
-          type="range"
-          min={0}
-          max={100}
-          value={value}
-          onChange={handleInputChange}
-        />
+      <div className="child-dice">
+        <div className="dice-numbers-div">
+          <h6 className="dice-numbers">0</h6>
+          <h6 className="dice-numbers">25</h6>
+          <h6 className="dice-numbers">50</h6>
+          <h6 className="dice-numbers">75</h6>
+          <h6 className="dice-numbers">100</h6>
+        </div>
+        <div className="parent-dice-range">
+          <input
+            className="dice-range"
+            type="range"
+            step={0.01}
+            min={0}
+            max={100}
+            value={value}
+            onChange={handleInputChange}
+          />
+        </div>
       </div>
+      <h1>Selected value is </h1>
+      <h1>{value.toFixed(2)}</h1>
     </div>
   );
 };
