5aebe7629fe611abd6f989c7be18167005576fbb
sangharsh <sangharshdev69@gmail.com>
2024-05-10 19:03:48 +0530

    hidding overFlow on the body inside diceWrapper

--- src/component/games/DiceWrapper/diceWrapper/diceWrapper.js
+++ src/component/games/DiceWrapper/diceWrapper/diceWrapper.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./DiceWrapper.css"
 import DiceBet from "../DiceBet/DiceBet.js"
 import Dice from '../dice/Dice.js';
+import { useLocation } from 'react-router-dom';
 
 const DiceWrapper = () => {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if(location.pathname === '/casino/dice'){
+      document.body.style.overflow = "hidden";
+    }else{
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    }
+  }, [location.pathname])
   return (
     <div className='diceWrapper'>
         <DiceBet />
